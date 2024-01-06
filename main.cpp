f381bbe09e1bc1c23f9dad257616465624884321
Krzysztof Koch <loczek3603@gmail.com>
2024-01-06 13:24:53 +0100

    Add main page description for Doxygen docummentation

--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
+/**
+ * \mainpage Object Storage Server source code documentation
+ *
+ * Object storage is an FTP/HTTP server for in-memory object storage and
+ * retrieval.
+ */
+
 int main(int argc, char *argv[]) { return 0; }
