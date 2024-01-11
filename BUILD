215aeafc66ccbc0737b07f0a1d38aa18a56d9bfd
Krzysztof Koch <loczek3603@gmail.com>
2024-01-11 13:49:07 +0100

    ExampleApp: Use command line arguments

--- BUILD
+++ BUILD
@@ -1,5 +1,8 @@
 cc_binary(
     name = "object_storage",
     srcs = ["example_app.cpp"],
-    deps = ["//server/object_storage"],
+    deps = [
+        "//server/object_storage",
+        "//utils",
+    ],
 )
--- example_app.cpp
+++ example_app.cpp
@@ -5,18 +5,43 @@
  * retrieval.
  */
 
+#include <cstdlib>
 #include <iostream>
+#include <string>
 
 #include "server/object_storage/src/object_storage.hpp"
+#include "utils/src/utils.hpp"
 
 using namespace server::object_storage;
 
 int main(int argc, char *argv[]) {
   // ObjectStorage server{"127.0.0.1", 1670, LogLevel::Debug};
-  ObjectStorage server{"127.0.0.1", 1670, LogLevel::Debug, true};
+
+  if (argc != 6) {
+    std::cout << "Usage ./object_storage <address> <port> <threads>  "
+                 "<auth|no_auth> <ftp_port_range>\n";
+    return -1;
+  }
+
+  // Parse command line arguments
+  const std::string address = argv[1];
+  const std::uint16_t port = std::atoi(argv[2]);
+  const auto thread_count = std::atoi(argv[3]);
+  const auto authenticate = std::string{argv[4]} == std::string{"auth"};
+  const auto ftp_port_range = utils::split(argv[5], "-");
+  std::uint16_t ftp_port_min = std::atoi(ftp_port_range[0].data());
+  std::uint16_t ftp_port_max = std::atoi(ftp_port_range[1].data());
+
+  // Instantiate the Object storage server
+  ObjectStorage server{
+      address, port, LogLevel::Info, authenticate, {ftp_port_min, ftp_port_max},
+  };
+
+  // Add users (for authentication)
   server.addUser("Nord", "VPN");
 
-  if (!server.start()) {
+  // Start the server with the given number of threads
+  if (!server.start(thread_count)) {
     return -1;
   }
 
